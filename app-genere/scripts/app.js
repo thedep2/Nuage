b38c1e4e2b107f5f198ce03e632304dab2ae6bbd
David <David@Colossus>
2015-05-13 00:03:22 +0200

    creation de la page principale au format desktop

---
+++ app-genere/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name nuageApp
+ * @description
+ * # nuageApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('nuageApp', []);
---
+++ app/scripts/app.js
@@ -0,0 +1,5 @@
+'use strict';
+
+/* App Module */
+
+var nuageApp = angular.module('nuageApp', []);
