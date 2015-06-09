2c59f732ed8dcaabfd2e2dbac026bda50cc4ace6
David Depix <dadepix-ext@airfrance.fr>
2015-06-09 16:49:48 +0200

    enlever angular faire parallax

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,28 +1,3 @@
 'use strict';
 
 /* App Module */
-
-var nuageApp = angular.module('nuageApp', [
-                                           'ngRoute',
-                                           'nuageControllers'    
-                                           ]);
-
-nuageApp.config(['$routeProvider',
-                 function($routeProvider) {
-	$routeProvider.
-	when('/index', {
-		templateUrl: 'views/acceuil.html',
-		controller: 'AcceuilController'
-	}).
-	when('/portfolio', {
-		templateUrl: 'views/portfolio.html',
-		controller: 'PortfolioController'
-	}).
-	when('/carousel', {
-		templateUrl: 'views/portfolio.html',
-		controller: 'PortfolioController'
-	}).
-	otherwise({
-		redirectTo: '/index'
-	});
-}]);
