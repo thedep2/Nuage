2d9448db46e35bdea39a93cf4795773b0960900d
David Depix <dadepix-ext@airfrance.fr>
2015-06-10 14:26:18 +0200

    responsive home page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,9 @@
 jQuery(document).ready(function() {
 	
-	if ($(window).width() > 768) {
+	
+	if ($(window).width() > 992) {
+		$('.parallax-bg').css('height','21em');
+		
 		$('#parallax .parallax-layer').parallax({
 			mouseport : $('#parallax')
 		});
@@ -8,6 +11,9 @@ jQuery(document).ready(function() {
 		jQuery('.parallax-layer')
 		.parallax({}, {xparallax: '0.05'}, {xparallax: '0.2'}, {xparallax: '0.3'}, {xparallax: '0.4'}, {xparallax: '0.6'}, {xparallax: '0.8'});
 		
+	} else {
+		$('.parallax-bg').css('height','24em');
+		$('#parallax').css('height','24em');
 	}
 		
 		
