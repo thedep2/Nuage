e2c9e79ab87215fbe465c83ddbbe2cd4884fec60
David Depix <dadepix-ext@airfrance.fr>
2015-06-10 13:08:25 +0200

    debut impl page astro

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,14 @@
-'use strict';
-
-/* App Module */
+jQuery(document).ready(function() {
+	
+	if ($(window).width() > 768) {
+		$('#parallax .parallax-layer').parallax({
+			mouseport : $('#parallax')
+		});
+		
+		jQuery('.parallax-layer')
+		.parallax({}, {xparallax: '0.05'}, {xparallax: '0.2'}, {xparallax: '0.3'}, {xparallax: '0.4'}, {xparallax: '0.6'}, {xparallax: '0.8'});
+		
+	}
+		
+		
+	});
